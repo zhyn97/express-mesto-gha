c9407d42df429c819b32a832c53a07d49b018fe6
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-03-18 15:42:13 +0100

    испраление v1.4

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use('/', cards);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.status(404).send({message: "Страница не найдена"})
+})
+
 app.listen(PORT, () => {
   console.log('Сервер запущен');
 });
